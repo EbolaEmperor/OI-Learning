b7f9d09ebb6250949ddcb851b7754c873754b1d7
EbolaEmperor <Ebola.ebola.pro>
2019-03-23 12:33:15 +0800

    update recent contest

--- RecentContest/noi2019_modal43/reo.cpp
+++ RecentContest/noi2019_modal43/reo.cpp
@@ -1,96 +1,70 @@
-// When I first wrote these, only I and God know what I'm doing.
-// Now, only God knows.
 #include<bits/stdc++.h>
 using namespace std;
 
 typedef pair<int,int> pii;
-const int N=1010;
-vector<int> anc[N],g[N];
-vector<pii> branch;
-set<int> son[N],split;
-bool cmp[N][N],lim2[N][N],isb[N],vis[N];
-int n,m1,m2,p[N],fa[N],tot=0,deg[N];
-int dep[N],idfn[N],odfn[N],dfc;
-
-void dfs0(int u)
-{
-    vis[u]=1;
-    p[++tot]=u;
-    for(int v : g[u])
-        if(!--deg[v]) dfs0(v);
-}
-
-void dfs(int u)
-{
-    idfn[u]=++dfc;
-    for(int v : son[u])
-        dep[v]=dep[u]+1,dfs(v);
-    odfn[u]=dfc;
-}
+const int N=1010,M=N*10;
+vector<int> g[N],nxt[N],tmp;
+bool vis1[N],vis2[N],no[N];
+int n,m1,m2,fa[N],rt;
+vector<pii> lim1,lim2;
 
 void dfs2(int u)
 {
-    for(int a : anc[u])
-        if(split.find(a)!=split.end())
-            {split.insert(u);break;}
-    for(int v : son[u]) dfs2(v);
+    vis2[u]=1;
+    tmp.emplace_back(u);
+    for(int v : g[u])
+        if(!vis1[v]&&!vis2[v]) dfs2(v);
+}
+
+void dfs1(int u,int la)
+{
+    memset(no,0,sizeof(no));
+    memset(vis2,0,sizeof(vis2));
+    tmp.clear();dfs2(u);
+    for(auto pr : lim1)
+        if(vis2[pr.first]&&vis2[pr.second])
+            no[pr.second]=1;
+    for(auto pr : lim2)
+        if(vis2[pr.first]&&vis2[pr.second])
+            no[pr.first]=1;
+    int cur;
+    for(int i=1;i<=n;i++)
+        if(vis2[i]&&!no[i]){cur=i;break;}
+    fa[cur]=la;
+    vis1[cur]=1;
+    nxt[cur]=tmp;
+    for(int v : nxt[cur])
+        if(!vis1[v]) dfs1(v,cur);
 }
 
 int main()
 {
     scanf("%d%d%d",&n,&m1,&m2);
     for(int i=1,u,v;i<=m1;i++)
-        scanf("%d%d",&u,&v),cmp[u][v]=1,anc[v].emplace_back(u);
-    for(int i=1,u,v;i<=m2;i++)
-        scanf("%d%d",&u,&v),lim2[u][v]=1;
-    for(int i=1;i<=n;i++)
-        for(int j=1;j<=n;j++)
-            if(lim2[i][j]&&lim2[j][i])
-            {
-                branch.emplace_back(pii(i,j));
-                lim2[i][j]=lim2[j][i]=0;
-                isb[i]=isb[j]=1;
-            }
-    for(int i=1;i<=n;i++)
-        for(int j=1;j<=n;j++)
-            if(lim2[i][j]) cmp[j][i]=1;
-    for(int i=1;i<=n;i++)
-        for(int j=1;j<=n;j++)
-            if(cmp[i][j]) g[i].emplace_back(j),deg[j]++;
-    for(int i=1;i<=n;i++)
-        if(!deg[i]&&!isb[i])
-            {dfs0(i);break;}
-    //for(int i=1;i<=tot;i++) cerr<<p[i]<<" ";cerr<<endl;
-    for(int i=1;i<=n;i++)
-        if(!vis[i]) dfs0(i);
-    //for(int i=1;i<=n;i++) cerr<<p[i]<<" ";cerr<<endl;
-    for(int i=2;i<=n;i++) fa[p[i]]=p[i-1],son[p[i-1]].insert(p[i]);
-    fa[p[1]]=0;dep[p[1]]=1;dfs(p[1]);
-    while(!branch.empty())
     {
-        sort(branch.begin(),branch.end(),[](pii a,pii b){return min(dep[a.first],dep[a.second])>min(dep[b.first],dep[b.second]);});
-        pii pr=branch.back();branch.pop_back();
-        int u=pr.first,v=pr.second;
-        if(dep[u]>dep[v]) swap(u,v);
-        if(idfn[v]<idfn[u]||idfn[v]>odfn[u]) continue;
-        split.clear();split.insert(v);dfs2(v);
-        static int tmp[N];int tot=0;
-        for(int x : split) tmp[++tot]=x;
-        sort(tmp+1,tmp+1+tot,[](int a,int b){return idfn[a]<idfn[b];});
-        for(int i=1;i<=tot;i++)
-        {
-            son[fa[tmp[i]]].erase(tmp[i]);
-            for(int x : son[tmp[i]])
-                fa[x]=fa[tmp[i]],son[fa[tmp[i]]].insert(x);
-        }
-        for(int i=2;i<=tot;i++) fa[tmp[i]]=tmp[i-1];
-        //cerr<<tmp[1]<<" "<<fa[u]<<endl;
-        fa[tmp[1]]=fa[u];
-        dfc=0;dfs(p[1]);
-        // for(int i=1;i<=n;i++)
-        //     printf("%d ",fa[i]);puts("");
+        scanf("%d%d",&u,&v);
+        lim1.emplace_back(pii(u,v));
+        g[u].emplace_back(v);
+        g[v].emplace_back(u);
+        no[v]=1;
+    }
+    for(int i=1,u,v;i<=m2;i++)
+    {
+        scanf("%d%d",&u,&v);
+        lim2.emplace_back(pii(u,v));
+        no[u]=1;
     }
     for(int i=1;i<=n;i++)
-        printf("%d ",fa[i]);puts("");
+        if(!no[i]){rt=i;break;}
+    for(int i=1;i<=n;i++)
+    {
+        if(i==rt) continue;
+        g[rt].emplace_back(i);
+        g[i].emplace_back(rt);
+        lim1.emplace_back(pii(rt,i));
+    }
+    dfs1(rt,0);
+    for(int i=1;i<=n;i++)
+        printf("%d ",fa[i]);
     return 0;
 }
