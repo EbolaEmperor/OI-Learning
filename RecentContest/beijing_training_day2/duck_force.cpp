6f62811b5a34f7105e0ae90293a248a883c1eecf
EbolaEmperor <40452651+EbolaEmperor@users.noreply.github.com>
2019-04-02 18:38:57 +0800

    update recent contest

--- RecentContest/beijing_training_day2/duck_force.cpp
+++ RecentContest/beijing_training_day2/duck_force.cpp
@@ -7,22 +7,22 @@ int f[N],cnt[N];
 
 int main()
 {
-	scanf("%d",&n);
-	for(int i=0;i<n;i++)
-	{
-		scanf("%d%d%d",l+i,r+i,v+i);
-		l[i]--;r[i]--;
-		len[i]=r[i]-l[i]+1;
-		len[i]=(1<<len[i])-1;
-	}
-	for(int s=1;s<(1<<n);s++)
-		cnt[s]=cnt[s^(s&-s)]+1;
-	for(int k=0;k<n;k++)
-		for(int s=0;s<(1<<n);s++)
-			if(cnt[s]==k)
-				for(int i=0;i<n;i++)
-					if(!((s>>i)&1)&&((s>>l[i])&len[i]))
-						f[s|1<<i]=max(f[s|1<<i],f[s]+v[i]);
-	printf("%d\n",f[(1<<n)-1]);
-	return 0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d%d%d",l+i,r+i,v+i);
+        l[i]--;r[i]--;
+        len[i]=r[i]-l[i]+1;
+        len[i]=(1<<len[i])-1;
+    }
+    for(int s=1;s<(1<<n);s++)
+        cnt[s]=cnt[s^(s&-s)]+1;
+    for(int k=0;k<n;k++)
+        for(int s=0;s<(1<<n);s++)
+            if(cnt[s]==k)
+                for(int i=0;i<n;i++)
+                    if(!((s>>i)&1)&&((s>>l[i])&len[i]))
+                        f[s|1<<i]=max(f[s|1<<i],f[s]+v[i]);
+    printf("%d\n",f[(1<<n)-1]);
+    return 0;
 }
