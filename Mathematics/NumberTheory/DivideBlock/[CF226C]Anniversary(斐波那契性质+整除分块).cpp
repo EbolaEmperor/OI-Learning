88fed874a8344f9279ca788047745794af5e7e2d
ebola <wchung.huang@gmail.com>
2024-10-11 23:52:22 +0800

    fuck

--- Mathematics/NumberTheory/DivideBlock/[CF226C]Anniversary(斐波那契性质+整除分块).cpp
+++ Mathematics/NumberTheory/DivideBlock/[CF226C]Anniversary(斐波那契性质+整除分块).cpp
@@ -2,24 +2,24 @@
 #include <cstring>
 using namespace std;
 
-int mod, l, r, k;
+long long mod, l, r, k;
 
 struct Matrix{
-    int a[2][2];
+    long long a[2][2];
     Matrix(){
         memset(a, 0, sizeof(a));
     }
-    int* operator [] (int i){return a[i];}
-    const int* operator [] (int i) const{return a[i];}
+    long long* operator [] (int i){return a[i];}
+    const long long* operator [] (int i) const{return a[i];}
     Matrix operator * (const Matrix &rhs) const{
         Matrix c;
         for(int i = 0; i < 2; i++)
             for(int j = 0; j < 2; j++)
                 for(int k = 0; k < 2; k++)
-                    c[i][j] = (c[i][j] + (long long)a[i][k] * rhs[j][k]) % mod;
+                    c[i][j] = (c[i][j] + a[i][k] * rhs[j][k]) % mod;
         return c;
     }
-    friend Matrix matpow(Matrix a, int n){
+    friend Matrix matpow(Matrix a, long long n){
         Matrix ans;
         ans[0][0] = ans[1][1] = 1;
         for(; n; n >>= 1, a = a * a)
@@ -28,16 +28,16 @@ struct Matrix{
     }
 };
 
-int getFib(int n){
+long long getFib(long long n){
     Matrix a;
     a[0][0] = a[0][1] = a[1][0] = 1;
     a = matpow(a, n - 1);
-    return a[0][0];
+    return a[0][0] % mod;
 }
 
-int getID(){
-    int maxgcd = 1;
-    for(int d = 1; d <= r; d++){
+long long getID(){
+    long long maxgcd = 1;
+    for(long long d = 1; d <= r; d++){
         d = min(r / (r / d), r);
         if(r / d - (l - 1) / d >= k) maxgcd = d;
     }
