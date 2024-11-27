95d9f850c547a7fc084c5b5922df5feba237eb3a
ebola <wchung.huang@gmail.com>
2024-11-27 12:02:32 +0800

    update

--- Interesting/wys_sort.cpp
+++ Interesting/wys_sort.cpp
@@ -3,12 +3,22 @@
 #define ui unsigned int
 using namespace std;
 
+// n 必须是 8 的倍数
 void wys_sort(ui *a, int n){
     static ui c[256];
     ui *tmp = new ui[n];
     for(int u = 0; u < 32; u += 8){
         memset(c, 0, 4 * 256);
-        for(int i = 0; i < n; i++) c[a[i]>>u&255]++;
+        for(int i = 0; i < n; i += 8){
+            ++c[a[i]>>u&255];
+            ++c[a[i+1]>>u&255];
+            ++c[a[i+2]>>u&255];
+            ++c[a[i+3]>>u&255];
+            ++c[a[i+4]>>u&255];
+            ++c[a[i+5]>>u&255];
+            ++c[a[i+6]>>u&255];
+            ++c[a[i+7]>>u&255];
+        }
         for(int i = 1; i < 256; i++) c[i] += c[i-1];
         for(int i = n - 1; i >= 0; i--) tmp[--c[a[i]>>u&255]] = a[i];
         memcpy(a, tmp, 4 * n);
@@ -18,7 +28,7 @@ void wys_sort(ui *a, int n){
 
 int main(){
     mt19937 rnd(19260817);
-    const int n = 1e8;
+    const int n = 2e8;
     ui *a = new ui[n];
     for(int i = 0; i < n; i++)
         a[i] = rnd();
