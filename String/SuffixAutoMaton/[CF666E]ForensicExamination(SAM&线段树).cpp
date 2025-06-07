cd6f2bf7cdff80e71e7cd91bf0e77f568daf1a77
Wenchong Huang <wchung.huang@gmail.com>
2025-06-08 00:05:30 +0800

    update

--- String/SuffixAutoMaton/[CF666E]ForensicExamination(SAM&线段树).cpp
+++ String/SuffixAutoMaton/[CF666E]ForensicExamination(SAM&线段树).cpp
@@ -1,44 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-namespace IO
-{
-	const int S=1<<20;
-	//Input Correlation
-	char buf[S],*H,*T;
-	inline char Get()
-	{
-		if(H==T) T=(H=buf)+fread(buf,1,S,stdin);
-		if(H==T) return -1;return *H++;
-	}
-	inline int read()
-	{
-		int x=0;char c=Get();
-		while(!isdigit(c)) c=Get();
-		while(isdigit(c)) x=x*10+c-'0',c=Get();
-		return x;
-	}
-	inline void reads(char *s)
-	{
-		char c=Get();int tot=0;
-		while(c<'a'||c>'z') c=Get();
-		while(c>='a'&&c<='z') s[++tot]=c,c=Get();
-		s[++tot]='\0';
-	}
-	//Output Correlation
-	char obuf[S],*oS=obuf,*oT=oS+S-1,c,qu[55];int qr;
-	inline void flush(){fwrite(obuf,1,oS-obuf,stdout);oS=obuf;}
-	inline void putc(char x){*oS++ =x;if(oS==oT) flush();}
-	template <class I>inline void print(I x)
-	{
-		if(!x) putc('0');
-		if(x<0) putc('-'),x=-x;
-		while(x) qu[++qr]=x%10+'0',x/=10;
-		while(qr) putc(qu[qr--]);
-	}
-}
-
-using namespace IO;
 const int N=1200010;
 int ch[N][27],prt[N],len[N];
 int m,lst=1,tot=1,topo[N];
@@ -76,7 +38,7 @@ void TopoSort()
 
 typedef pair<int,int> pii;
 int ls[N*22],rs[N*22],sz=0;
-pii data[N*22];
+pii node[N*22];
 
 pii operator + (const pii &a,const pii &b)
 {
@@ -88,29 +50,29 @@ pii operator + (const pii &a,const pii &b)
 void Insert(int &o,int k,int l=1,int r=m)
 {
 	if(!o) o=++sz;
-	if(l==r){data[o]=pii(l,data[o].second+1);return;}
+	if(l==r){node[o]=pii(l,1);return;}
 	int mid=(l+r)/2;
 	if(k<=mid) Insert(ls[o],k,l,mid);
 	else Insert(rs[o],k,mid+1,r);
-	data[o]=data[ls[o]]+data[rs[o]];
+	node[o]=node[ls[o]]+node[rs[o]];
 }
 
 int Merge(int L,int R,int l=1,int r=m)
 {
 	if(!L||!R) return L+R;
 	int o=++sz;
-	if(l==r){data[o]=pii(l,data[L].second+data[R].second);return o;}
+	if(l==r){node[o]=pii(l,node[L].second+node[R].second);return o;}
 	int mid=(l+r)/2;
 	ls[o]=Merge(ls[L],ls[R],l,mid);
 	rs[o]=Merge(rs[L],rs[R],mid+1,r);
-	data[o]=data[ls[o]]+data[rs[o]];
+	node[o]=node[ls[o]]+node[rs[o]];
 	return o;
 }
 
 pii Query(int nl,int nr,int o,int l=1,int r=m)
 {
 	if(!o) return pii(0,0);
-	if(l==nl&&r==nr) return data[o];
+	if(l==nl&&r==nr) return node[o];
 	int mid=(l+r)/2;
 	if(nr<=mid) return Query(nl,nr,ls[o],l,mid);
 	if(nl>mid) return Query(nl,nr,rs[o],mid+1,r);
@@ -141,32 +103,30 @@ int find(int l,int r)
 
 int main()
 {
-	reads(s);
+	scanf("%s", s+1);
 	int len=strlen(s+1);
 	for(int i=1;i<=len;i++)
 		Extend(s[i]-'a'),pos[i]=lst;
 	Extend(26);
-	m=read();
+	scanf("%d", &m);
 	for(int i=1;i<=m;i++)
 	{
-		reads(b);
+		scanf("%s", b+1);
 		len=strlen(b+1);
 		for(int j=1;j<=len;j++)
 			Extend(b[j]-'a'),Insert(root[lst],i);
 		Extend(26);
 	}
 	Init();
-	int Q=read(),l,r,pl,pr;
+	int Q,l,r,pl,pr;
+	scanf("%d", &Q);
 	while(Q--)
 	{
-		l=read();r=read();
-		pl=read();pr=read();
+		scanf("%d%d%d%d", &l, &r, &pl, &pr);
 		int x=find(pl,pr);
 		pii ans=Query(l,r,root[x]);
 		if(!ans.second) ans.first=l;
-		print(ans.first);putc(' ');
-		print(ans.second);putc('\n');
+		printf("%d %d\n", ans.first, ans.second);
 	}
-	flush();
 	return 0;
 }
